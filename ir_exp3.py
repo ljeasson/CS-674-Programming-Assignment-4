b7855922700253e009556231da84628ef590009d
Lee Easson <ljeasson@gmail.com>
2019-12-02 23:11:40 -0800

    Add processing loop Inverse and Wiener filtering

--- ir_exp3.py
+++ ir_exp3.py
@@ -22,11 +22,12 @@ def box_muller(stdev, mean):
     return Z0 * stdev + mean
 
 def H(u,v,T,a,b):
-    p1 = T / (pi * (u*a + v*b))
-    p2 = sin(pi * (u*a + v*b))
-    p3 = cos(u*a + v*b) + sin(u*a + v*b)
+    H = 1
+    muf = pi * (u*a + v*b)
+    if muf != 0.0: 
+        H = (T / muf) * sin(muf) * (cos(muf) - sin(muf))
+    return H
 
-    return p1 * p2 * p3
 
 # Experiment 3 (Image Restoration - motion blur)
 F = PGMImage("images/lenna.pgm")
@@ -34,12 +35,22 @@ F_pixels = []
 for row in F.pixels:
     F_pixels.append([i for i in row])
 
-# Apply motion blur to lenna
-a = b = 0.1
-T = 1
+
+# TODO: Apply motion blur to lenna
 G = PGMImage("images/lenna.pgm")
+G_pixels = []
+for row in F.pixels:
+    G_pixels.append([i for i in row])
+
+for row in range(len(G_pixels)):
+    for column in range(len(G_pixels[row])):
+        G_pixels[row][column] = F_pixels[row][column] * H(row, column, T=1, a=0.1, b=0.1)
+
+G.pixels = G_pixels
+G.truncate_intensity_values()
 
 G.save(f"Motion-blur-{F.name}")
+
 
 # Apply Gaussian noise to motion-blurred lenna
 for i in (1, 10, 100):
@@ -58,6 +69,40 @@ for i in (1, 10, 100):
 
     G_noisy.save(f"Motion-blurred-Gaussian-noise-stdev{i}-{F.name}")
 
+
 # TODO: Apply Inverse Filtering to each degraded image
+def inverse_filtering(image, r):
+    inv_filtered = PGMImage(image)
+    inv_filtered_pixels = []
+    for row in inv_filtered.pixels:
+        inv_filtered_pixels.append([i for i in row])
+
+    inv_filtered.pixels = inv_filtered_pixels
+    inv_filtered.truncate_intensity_values()
+
+    inv_filtered.save(f"Inverse_filtered-{r}-{inv_filtered.name}")
+
+for img in ("Motion-blurred-Gaussian-noise-stdev1-lenna.pgm", 
+        "Motion-blurred-Gaussian-noise-stdev10-lenna.pgm",
+        "Motion-blurred-Gaussian-noise-stdev100-lenna.pgm"):
+    for r in (40, 70, 85):
+        inverse_filtering(img, r)
+
 
 # TODO: Apply Wiener Filtering to each degraded image
+def wiener_filtering(image, k):
+    wiener_filtered = PGMImage(image)
+    wiener_filtered_pixels = []
+    for row in wiener_filtered.pixels:
+        wiener_filtered_pixels.append([i for i in row])
+
+    wiener_filtered.pixels = wiener_filtered_pixels
+    wiener_filtered.truncate_intensity_values()
+
+    wiener_filtered.save(f"Wiener_filtered-{k}-{wiener_filtered.name}")
+
+for img in ("Motion-blurred-Gaussian-noise-stdev1-lenna.pgm", 
+        "Motion-blurred-Gaussian-noise-stdev10-lenna.pgm",
+        "Motion-blurred-Gaussian-noise-stdev100-lenna.pgm"):
+    for k in (1, 10, 20, 50, 100):
+        wiener_filtering(img, k)
